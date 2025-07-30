f42c45ce239c1804005ecb568774da275ea05a37
mandraw <M_Mandrake@hotmail.fr>
2025-07-31 00:43:41 +0200

    Refactor Myriad System: Update characteristics, item properties, and templates; add French localization support

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -1,23 +1,34 @@
 export const MYRIAD_SYSTEM = {};
 
 /**
- * The set of Ability Scores used within the system.
+ * The set of Characteristics used within the system.
  * @type {Object}
  */
-MYRIAD_SYSTEM.abilities = {
-  str: 'MYRIAD_SYSTEM.Ability.Str.long',
-  dex: 'MYRIAD_SYSTEM.Ability.Dex.long',
-  con: 'MYRIAD_SYSTEM.Ability.Con.long',
-  int: 'MYRIAD_SYSTEM.Ability.Int.long',
-  wis: 'MYRIAD_SYSTEM.Ability.Wis.long',
-  cha: 'MYRIAD_SYSTEM.Ability.Cha.long',
+MYRIAD_SYSTEM.characteristics = {
+  puissance: "MYRIAD_SYSTEM.Characteristic.Puissance",
+  coordination: "MYRIAD_SYSTEM.Characteristic.Coordination",
+  ingeniosite: "MYRIAD_SYSTEM.Characteristic.Ingeniosite",
+  perspicacite: "MYRIAD_SYSTEM.Characteristic.Perspicacite"
 };
 
-MYRIAD_SYSTEM.abilityAbbreviations = {
-  str: 'MYRIAD_SYSTEM.Ability.Str.abbr',
-  dex: 'MYRIAD_SYSTEM.Ability.Dex.abbr',
-  con: 'MYRIAD_SYSTEM.Ability.Con.abbr',
-  int: 'MYRIAD_SYSTEM.Ability.Int.abbr',
-  wis: 'MYRIAD_SYSTEM.Ability.Wis.abbr',
-  cha: 'MYRIAD_SYSTEM.Ability.Cha.abbr',
+/**
+ * The set of Magic Domains available in the system
+ * @type {Object}
+ */
+MYRIAD_SYSTEM.magicDomains = {
+  elements: "MYRIAD_SYSTEM.MagicDomain.Elements",
+  corps: "MYRIAD_SYSTEM.MagicDomain.Corps",
+  immateriel: "MYRIAD_SYSTEM.MagicDomain.Immateriel"
+};
+
+/**
+ * Item properties
+ * @type {Object}
+ */
+MYRIAD_SYSTEM.itemProperties = {
+  utilisable: "MYRIAD_SYSTEM.ItemProperty.Utilisable",
+  equipable: "MYRIAD_SYSTEM.ItemProperty.Equipable",
+  magique: "MYRIAD_SYSTEM.ItemProperty.Magique",
+  consommable: "MYRIAD_SYSTEM.ItemProperty.Consommable",
+  jetable: "MYRIAD_SYSTEM.ItemProperty.Jetable"
 };
